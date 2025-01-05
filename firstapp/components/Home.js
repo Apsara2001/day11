298aeaabe422e8bbbfcaab4a29dd309a7a42d062
Apsara2001 <apsaraduanji2001@gmail.com>
2025-01-05 18:20:55 +0530

    Initialize React Native project with basic dependencies

---
+++ firstapp/components/Home.js
@@ -0,0 +1,3 @@
+import { useNavigation } from '@react-navigation/native';
+import { StyleSheet, View, ScrollView } from 'react-native';
+import { PaperProvider, Text, Divider, Button } from 'react-native-paper';
